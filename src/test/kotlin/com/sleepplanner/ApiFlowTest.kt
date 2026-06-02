850f59c8fd70410937238f68fc119aa94c26caa4
zobr <zobr.akazup@gmail.com>
2026-06-02 22:09:26 +0300

    fix import

--- src/test/kotlin/com/sleepplanner/ApiFlowTest.kt
+++ src/test/kotlin/com/sleepplanner/ApiFlowTest.kt
@@ -1,6 +1,5 @@
 package com.sleepplanner
 
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
